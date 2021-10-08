36638e215b4173331eed22e39318b0fe8bf3b79b
Anktony <antoneslima@gmail.com>
2021-10-08 02:19:05 -0300

    modulo 8 - objetos e métodos

--- Javascript - Básico/script.js
+++ Javascript - Básico/script.js
@@ -1,3 +1,13 @@
-if(1==1)
-alert("dale")
-console.log("cu")
+var nomeArray = [1, 2, 3, 4, 5, 6, 7];
+ 
+var dale = 0;
+
+
+for(var i in nomeArray){
+    dale += nomeArray[i];
+}
+
+for(var i of nomeArray){console.log(i)};
+
+alert(dale);
+
