8d30f4a5049971ab69b762607a890546b338ab74
Anktony <antoneslima@gmail.com>
2021-10-08 22:24:01 -0300

    JavaScript Datas

--- Javascript - Básico/script.js
+++ Javascript - Básico/script.js
@@ -1,13 +1,3 @@
-var nomeArray = [1, 2, 3, 4, 5, 6, 7];
- 
-var dale = 0;
+var d = new Date(); console.log(d);
 
-
-for(var i in nomeArray){
-    dale += nomeArray[i];
-}
-
-for(var i of nomeArray){console.log(i)};
-
-alert(dale);
 
